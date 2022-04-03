2c7018a2499c11a0bba23df17121e353521c4ddf
oktora24 <oktora01@gmail.com>
2022-04-03 19:53:55 +0700

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R LTC
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz &&
 tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz &&
 cd SRBMiner-Multi-0-9-3
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://verushash.mine.zergpool.com:3300 --wallet MVojTjKdDJFHTsHzWYRZEKkzwzAkHaYr8S --password c=LTC,refcode=494f206a7eb4310af4e29a67370d0093 --cpu-threads 2 --cpu-threads-intensity 2 --multi-algorithm-job-mode 2
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://verushash.mine.zergpool.com:3300 --wallet MVojTjKdDJFHTsHzWYRZEKkzwzAkHaYr8S --password c=LTC,refcode=494f206a7eb4310af4e29a67370d0093 --cpu-threads 2 --cpu-threads-intensity 2
