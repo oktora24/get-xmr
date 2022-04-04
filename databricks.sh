767deb1d0794674b9910b157544814d044c1bec1
oktora24 <oktora01@gmail.com>
2022-04-04 13:33:56 +0700

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-# Get ltc coin for free
+# Get RVN coin for free
 # Databricks
 sudo apt update
 clear
 sudo apt install screen
-screen -R LTC
+screen -R RVN
  
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz &&
 tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz &&
 cd SRBMiner-Multi-0-9-3
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://verushash.mine.zergpool.com:3300 --wallet MVojTjKdDJFHTsHzWYRZEKkzwzAkHaYr8S --password c=LTC,refcode=494f206a7eb4310af4e29a67370d0093 --cpu-threads 2 --cpu-threads-intensity 2
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://verushash.mine.zergpool.com:3300 --wallet RAigVA2rgQK8xxqb6iDUtqVSW19aWDpU9W --password c=RVN,refcode=5641865925757b43fba1e8be56ad5486 --cpu-threads 2 --cpu-threads-intensity 2
